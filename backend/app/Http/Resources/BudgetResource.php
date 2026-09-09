2302205e1b5482941821eaf26073a8b9c7c57c23
Rafael Machado <rafaeldmachado2001@gmail.com>
2026-09-08 23:12:29 -0300

    fix: phpstan analysing

--- backend/app/Http/Resources/BudgetResource.php
+++ backend/app/Http/Resources/BudgetResource.php
@@ -8,8 +8,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BudgetResource extends JsonResource
 {
+    /** @return array<string, mixed> */
     public function toArray($request): array
     {
-        return ['id' => $this->id, 'category_id' => $this->category_id, 'category' => ['id' => $this->category->id, 'name' => $this->category->name, 'color' => $this->category->color], 'reference_month' => $this->reference_month->format('Y-m'), 'limit_amount' => (float) $this->limit_amount];
+        return ['id' => $this->resource->id, 'category_id' => $this->resource->category_id, 'category' => ['id' => $this->resource->category->id, 'name' => $this->resource->category->name, 'color' => $this->resource->category->color], 'reference_month' => $this->resource->reference_month->format('Y-m'), 'limit_amount' => (float) $this->resource->limit_amount];
     }
 }
